efee562c645e802e90322784d4d4ac14fdae6cdf
Razeonic <39726375+Razeonic@users.noreply.github.com>
2020-01-16 12:26:37 +0100

    Update yeet.cs

--- Assets/Script test/yeet.cs
+++ Assets/Script test/yeet.cs
@@ -7,7 +7,7 @@ public class yeet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        fuck dig
+        fuck dig BO
     }
 
     // Update is called once per frame
